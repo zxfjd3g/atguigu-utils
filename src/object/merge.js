6dc3ece6d2ab966ef8acdf68141c46a99d05a199
zxfjd3g <258147149@qq.com>
2020-01-09 22:26:58 +0800

    merge

--- src/object/merge.js
+++ src/object/merge.js
@@ -1,5 +1,8 @@
 /* 
 merge(...objs): 合并多个对象, 返回一个合并后对象(不改变原对象)
+{ a: [{ x: 2 }, { y: 4 }], b: 1}
+{ a: { z: 3}, b: [2, 3], c: 'foo'}
+合并后: { a: [ { x: 2 }, { y: 4 }, { z: 3 } ], b: [ 1, 2, 3 ], c: 'foo' }
 */
 export function mergeObject(...objs) {
   return objs.reduce((pre, obj) => {
@@ -7,6 +10,5 @@ export function mergeObject(...objs) {
       p[key] = !p.hasOwnProperty(key) ? obj[key] : [].concat(p[key], obj[key])
       return p
     }, pre)
-  
   }, {})
 }
